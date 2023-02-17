4509b493ea4337c49513ab8b6711557a29ecddeb
samdunk47 <98485063+samdunk47@users.noreply.github.com>
2023-02-17 21:35:16 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+"use strict";
+let message = "Hello World";
+console.log(message);
+const element = document.getElementById("text");
+if (element) {
+    element.textContent = "this is a test yooo";
+}
+//# sourceMappingURL=script.js.map
---
+++ ts/script.ts
@@ -0,0 +1,7 @@
+let message:string = "Hello World";
+console.log(message);
+
+const element = document.getElementById("text")
+if (element) {
+    element.textContent = "this is a test yooo"
+}
