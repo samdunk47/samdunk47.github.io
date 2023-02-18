6bfa3ea0b86fbebcfb800f828b0b15347c60b516
samdunk47 <98485063+samdunk47@users.noreply.github.com>
2023-02-18 22:04:44 +0000

    new

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 "use strict";
-let message = "Hello World";
-console.log(message);
+const board = document.getElementById("board");
+const cells = [];
+for (let i = 0; i < 6; i++) {
+    for (let j = 0; j < 5; j++) {
+        let cell = document.createElement("div");
+        let cellID = "cell " + i.toString() + j.toString();
+        cell.className = cellID;
+        cells.push(cell);
+        board.appendChild(cell);
+    }
+}
 //# sourceMappingURL=script.js.map
--- ts/script.ts
+++ ts/script.ts
@@ -1,2 +1,12 @@
-let message:string = "Hello World";
-console.log(message);
+const board = document.getElementById("board");
+const cells: HTMLElement[] = [];
+
+for (let i = 0; i < 6; i++) {
+    for (let j = 0; j < 5; j++) {
+        let cell = document.createElement("div");
+        let cellID = "cell " + i.toString() + j.toString();
+        cell.className = cellID;
+        cells.push(cell)
+        board!.appendChild(cell)
+    }
+}
