a76f12cdb9a526f87c875ed75a5015559b2fba54
samdunk47 <98485063+samdunk47@users.noreply.github.com>
2023-02-25 21:39:01 +0000

    today

--- js/script.js
+++ js/script.js
@@ -1,10 +1,57 @@
 "use strict";
 const board = document.getElementById("board");
-const findCell = (id) => {
-    let cellFound = document.getElementById("cell-" + id);
-    return cellFound;
+let currentRow = 1;
+const createCells = () => {
+    for (let i = 1; i <= 6; i++) {
+        const row = document.createElement("div");
+        row.className = "row " + i.toString();
+        for (let j = 1; j <= 5; j++) {
+            const cell = document.createElement("div");
+            cell.className = "cell " + i.toString() + j.toString();
+            const paragraph = document.createElement("p");
+            paragraph.className = "letter";
+            paragraph.innerHTML = "A";
+            cell.appendChild(paragraph);
+            row.appendChild(cell);
+        }
+        board === null || board === void 0 ? void 0 : board.appendChild(row);
+    }
+};
+createCells();
+const rows = document.querySelectorAll(".row");
+const findNextEmptyCellInRow = () => {
+    var _a;
+    for (let cell of (_a = rows[currentRow - 1]) === null || _a === void 0 ? void 0 : _a.children) {
+        const cellParagraph = cell.children[0];
+        const cellText = cellParagraph.innerHTML;
+        const cellId = cell.classList[1];
+        return cellId;
+    }
+    return "";
+};
+const onEnterPress = () => {
+    console.log("enter");
+};
+const onBackspacePress = () => {
+    console.log("backspace");
+};
+const onKeyPress = (letter) => {
+    letter = letter.toUpperCase();
+    const emptyCellId = findNextEmptyCellInRow();
+    const emptyCell = document.getElementsByClassName(emptyCellId)[0];
 };
 document.addEventListener("keydown", (event) => {
-    console.log(event);
+    let key = event.key;
+    if (key == "Enter") {
+        onEnterPress();
+    }
+    else if (key == "Backspace") {
+        onBackspacePress();
+    }
+    else if (((key.charCodeAt(0) >= 97 && key.charCodeAt(0) <= 122) ||
+        (key.charCodeAt(0) >= 65 && key.charCodeAt(0) <= 90)) &&
+        key.length === 1) {
+        onKeyPress(key);
+    }
 });
 //# sourceMappingURL=script.js.map
--- ts/script.ts
+++ ts/script.ts
@@ -1,19 +1,66 @@
 const board = document.getElementById("board");
 let currentRow: number = 1;
 
-const findCell = (id: string) => {
-    let cellFound = document.getElementById("cell-" + id);
-    return cellFound;
+const createCells = () => {
+    for (let i = 1; i <= 6; i++) {
+        const row = document.createElement("div");
+        row.className = "row " + i.toString();
+
+        for (let j = 1; j <= 5; j++) {
+            const cell = document.createElement("div");
+            cell.className = "cell " + i.toString() + j.toString();
+
+            const paragraph = document.createElement("p");
+            paragraph.className = "letter";
+            paragraph.innerHTML = "A"
+
+            cell.appendChild(paragraph);
+            row.appendChild(cell);
+        }
+        board?.appendChild(row);
+    }
+};
+
+createCells();
+
+const rows = document.querySelectorAll(".row");
+
+const findNextEmptyCellInRow = (): string => {
+    for (let cell of rows[currentRow - 1]?.children) {
+        const cellParagraph: Element = cell.children[0];
+        const cellText: string = cellParagraph.innerHTML;
+        const cellId: string = cell.classList[1];
+        return cellId;
+    }
+    return "";
 };
 
 const onEnterPress = () => {
-    
+    console.log("enter");
 };
 
 const onBackspacePress = () => {
+    console.log("backspace");
+};
 
+const onKeyPress = (letter: string) => {
+    letter = letter.toUpperCase();
+    const emptyCellId: string = findNextEmptyCellInRow();
+    const emptyCell = document.getElementsByClassName(emptyCellId)[0];
 };
 
 document.addEventListener("keydown", (event) => {
-    console.log(event);
+    let key: string = event.key;
+
+    if (key == "Enter") {
+        onEnterPress();
+    } else if (key == "Backspace") {
+        onBackspacePress();
+    } else if (
+        ((key.charCodeAt(0) >= 97 && key.charCodeAt(0) <= 122) ||
+            (key.charCodeAt(0) >= 65 && key.charCodeAt(0) <= 90)) &&
+        key.length === 1
+    ) {
+        onKeyPress(key);
+    }
 });
